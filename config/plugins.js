d40f9412b2e10c388a289ecdddd8a8dde315e1db
Alexandru-Vlad Micu <vlad15235@gmail.com>
2023-08-16 17:56:08 +0300

    Added nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,25 @@ module.exports = ({ env }) => ({
     'users-permissions': {
 		config: {
 			jwt: {
-				expiresIn: '37d',
+				expiresIn: '9999999d',
 			},
 		},
+    },
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.GMAIL_APP_PASS
+                }
+                // ... any custom nodemailer options
+            },
+            settings: {
+                defaultFrom: process.env.EMAIL,
+                defaultReplyTo: process.env.EMAIL,
+            },
+        },
     },
 })
